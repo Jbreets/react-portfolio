840632ecfd4277ec674981514db4fc436090e826
JackBreeton <jackbreeton@gmail.com>
2025-08-20 09:08:29 +0100

    updated poster image

--- src/components/images/index.js
+++ src/components/images/index.js
@@ -1,11 +1,11 @@
-// ../components/images/index.js
+
 import ultraTickets from './ultra-tickets.png';
 import ultraSites from './ultra-sites.png';
 import ultraGamesScoreboard from './ultra-games-scoreboard.png';
 import scraper from './scraper.png';
 import kitTracker from './kit-tracker.png';
 import pdfGen from './pdf-gen.png';
-import qr from './qr.png';
+import qr from './poster-gen.png';
 import totals from './totals.png';
 import csharpCalculator from './csharpCalculator.png';
 import employeeManagerApp from './employee-management.png';
